f7f62f67a060bca526a448a36f815e501bd12c8a
Jordan Littlefair <jordanlittlefair@fsmail.net>
2016-02-07 01:02:09 +0000

    Create Xcode workspace and "Core" project.

--- Code/Core/PlatformDefinitions.hpp
+++ Code/Core/PlatformDefinitions.hpp
@@ -13,15 +13,13 @@
 */
 
 #ifdef _WIN32
-#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::Windows)
-#endif
-
-#ifdef MAC	// TODO- Mac defines
-#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::Mac)
+	#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::Windows)
+#elif defined __APPLE__
+	#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::Mac)
 #endif
 
 #ifndef FND_PLATFORM_CURRENT_OS
-#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::UnknownOS)
+	#define FND_PLATFORM_CURRENT_OS (Fnd::Core::Platform::OS::Unknown)
 #endif
 
 /*
@@ -30,18 +28,16 @@
 
 #ifdef _WIN32
 	#ifdef _WIN32
-	#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::B32)
+		#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::B32)
+	#elif defined _WIN64
+		#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::B64)
 	#endif
-
-	#ifdef _WIN64
+#elif defined __APPLE__
 	#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::B64)
-	#endif
 #endif
 
-// TODO- Mac 
-
 #ifndef FND_PLATFORM_CURRENT_BIT
-#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::UnknownBit)
+#define FND_PLATFORM_CURRENT_BIT (Fnd::Core::Platform::Bit::Unknown)
 #endif
 
 /*
@@ -56,5 +52,5 @@
 
 
 #ifndef FND_PLATFORM_CURRENT_BUILD
-#define FND_PLATFORM_CURRENT_BUILD (Fnd::Core::Platform::Build::UnknownBuild)
+#define FND_PLATFORM_CURRENT_BUILD (Fnd::Core::Platform::Build::Unknown)
 #endif
