4208cfff0ac936ae71440bb671b109a18372457f
Jordan Littlefair <jordanlittlefair@fsmail.net>
2016-08-07 23:05:47 +0100

    Add UtilityTests project and TestSuite

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -1,29 +1,41 @@
 #include "../Framework/TestSuite.hpp"
-#include "ExampleTestSuite.hpp"
 #include "../Framework/ResultPrinterCollection.hpp"
 #include "../Framework/ConsoleResultPrinter.hpp"
 #include "../Framework/XmlResultPrinter.hpp"
 
-#include <iostream>
+#include "../UtilityTests/UtilityTestSuite.hpp"
+
+
+#include <cassert>
 
 using namespace Fnd::Test;
 
 int main()
 {
-	{
-	ExampleTestSuite test_suite;
+	std::vector<TestSuitePtr> test_suites;
 	
 	std::shared_ptr<ResultPrinterCollection> result_printers = std::make_shared<ResultPrinterCollection>();
 	
 	result_printers->AddResultPrinter(std::make_shared<ConsoleResultPrinter>());
 	result_printers->AddResultPrinter(std::make_shared<XmlResultPrinter>("TestResults/FndTestResults"));
-
-	test_suite.SetResultPrinter(result_printers);
 	
-	test_suite.Run();
 	
-	TestSuiteResult result = test_suite.GetResult();
+	/*
+		Add any TestSuites below
+	 */
+	
+	test_suites.push_back( std::make_shared<Utility::UtilityTestSuite>() );
+	
+	
+	
+	for (auto& test_suite : test_suites)
+	{
+		test_suite->SetResultPrinter(result_printers);
 		
+		test_suite->Run();
+		
+		assert( test_suite->GetResult().GetSucceeded() );
 	}
+	
 	return 0;
 }
---
+++ Test/UtilityTests/UtilityTestSuite.cpp
@@ -0,0 +1,8 @@
+#include "UtilityTestSuite.hpp"
+
+using namespace Fnd::Test::Utility;
+
+UtilityTestSuite::UtilityTestSuite():
+	TestSuite("UtilityTestSuite")
+{
+}
