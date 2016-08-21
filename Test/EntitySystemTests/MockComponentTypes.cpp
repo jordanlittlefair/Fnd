35ae50c10423875a9149e40c1d56220c3ed3ba50
Jordan Littlefair <jordanlittlefair@fsmail.net>
2016-08-22 00:48:59 +0100

    Node and Component types now use auto generated Ids
    Still need to remove the Ids completely so only the name is required…

--- Test/EntitySystemTests/MockComponentTypes.cpp
+++ Test/EntitySystemTests/MockComponentTypes.cpp
@@ -1,10 +1,12 @@
 #include "MockComponentTypes.hpp"
 
+#include "../../Code/EntitySystem/StaticIdNameMappers.hpp"
+
 using namespace Fnd::Test::EntitySystem;
 using namespace Fnd::EntitySystem;
 
-const ComponentId MockComponentType1::Id = 1;
-const ComponentId MockComponentType2::Id = 2;
-
 const std::string MockComponentType1::Name = "One";
 const std::string MockComponentType2::Name = "Two";
+
+const ComponentId MockComponentType1::Id = StaticIdNameMappers::Instance().GetComponentIdNameMapper().GenerateId(MockComponentType1::Name);
+const ComponentId MockComponentType2::Id = StaticIdNameMappers::Instance().GetComponentIdNameMapper().GenerateId(MockComponentType2::Name);
--- Test/EntitySystemTests/MockNodeTypes.cpp
+++ Test/EntitySystemTests/MockNodeTypes.cpp
@@ -1,13 +1,16 @@
 #include "MockNodeTypes.hpp"
 
+#include "../../Code/EntitySystem/StaticIdNameMappers.hpp"
+
 using namespace Fnd::Test::EntitySystem;
 using namespace Fnd::EntitySystem;
 
-const NodeId MockNodeType1::Id = 1;
-const NodeId MockNodeType2::Id = 2;
 
 const std::string MockNodeType1::Name = "One";
 const std::string MockNodeType2::Name = "Two";
+
+const NodeId MockNodeType1::Id = StaticIdNameMappers::Instance().GetNodeIdNameMapper().GenerateId(MockNodeType1::Name);
+const NodeId MockNodeType2::Id = StaticIdNameMappers::Instance().GetNodeIdNameMapper().GenerateId(MockNodeType2::Name);
 
 /* static */
 auto MockNodeType1::GetComponents(const Fnd::EntitySystem::EntityId& entity_id, Fnd::EntitySystem::ComponentManager& component_manager) -> Components
