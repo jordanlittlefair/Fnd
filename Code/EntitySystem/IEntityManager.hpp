f0e79001ce970def7584a503feb3261ef33d612b
Jordan Littlefair <jordanlittlefair@fsmail.net>
2016-09-27 01:29:56 +0100

    Add IEntityManager interface for consistency

---
+++ Code/EntitySystem/IEntityManager.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "EntityId.hpp"
+
+#include <vector>
+
+namespace Fnd
+{
+
+namespace EntitySystem
+{
+
+class IEntityManager
+{
+public:
+	
+	virtual EntityId CreateEntity() = 0;
+	
+	virtual bool HasEntity(const EntityId& entity_id) const = 0;
+	
+	virtual void DestroyEntity(const EntityId& entity_id) = 0;
+	
+	virtual ~IEntityManager() {}
+};
+
+}
+
+}
